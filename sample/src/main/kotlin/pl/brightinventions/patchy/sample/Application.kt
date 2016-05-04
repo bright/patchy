df353207c46450773f361e4b4c4d1ebdd2ec44d2
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2016-05-04 18:29:09 +0200

    Hide web mvc configurer

--- sample/src/main/kotlin/pl/brightinventions/patchy/sample/Application.kt
+++ sample/src/main/kotlin/pl/brightinventions/patchy/sample/Application.kt
@@ -1,16 +1,36 @@
 package pl.brightinventions.patchy.sample
 
+import org.hibernate.validator.constraints.NotBlank
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Import
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import pl.brightinventions.patchy.PatchyConfiguration
+import pl.brightinventions.patchy.PatchyRequest
+import pl.brightinventions.patchy.getValue
+import javax.validation.Valid
 
+
+data class Request(override var _changes:Map<String,Any?> = mapOf<String,Any?>()) : PatchyRequest {
+    @get:NotBlank
+    val name:String? by { _changes }
+}
 
 @RestController
 class PatchingCtrl {
-
+    @RequestMapping("/", method = arrayOf(RequestMethod.PATCH))
+    fun update(@Valid @RequestBody request: Request){
+        println(request)
+    }
 }
 
 @SpringBootApplication
+@EnableWebMvc
+@Import(PatchyConfiguration::class)
 open class Application {
     companion object {
         @JvmStatic
--- src/main/kotlin/pl/brightinventions/patchy/PatchyConfiguration.kt
+++ src/main/kotlin/pl/brightinventions/patchy/PatchyConfiguration.kt
@@ -1,18 +1,22 @@
 package pl.brightinventions.patchy
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @EnableWebMvc
-open class PatchyConfiguration : WebMvcConfigurerAdapter() {
-    @Autowired
-    lateinit var messageConverters : HttpMessageConverters
+open class PatchyConfiguration {
+    @Bean
+    open fun patchyMvcSupport(messageConverters: HttpMessageConverters): WebMvcConfigurer =
+            PatchyMvcConfigurer(messageConverters)
+}
 
+internal class PatchyMvcConfigurer(val messageConverters: HttpMessageConverters) : WebMvcConfigurerAdapter() {
     override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver>) {
         argumentResolvers += PatchyRequestHandlerMethodArgumentResolver(messageConverters.toList(), null)
     }
